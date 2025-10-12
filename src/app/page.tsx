a93939572cb75f7b46e98dc6b3e1362e002bd84a
Kamran Kiani <kiani.km@gmail.com>
2025-10-12 12:25:18 +0330

    Remove Prisma seed script and update schema output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
-  const media = await prisma.media.findMany();
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-">
-      <h1>Media Items: {media.length}</h1>
-    </main>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-"></main>
   );
 }
