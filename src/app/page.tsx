b994008ba5733f522ab643b35f0f90d0340ad1bb
Kamran Kiani <kiani.km@gmail.com>
2025-10-12 17:22:09 +0330

    Add media count display to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
+  const media = await prisma.media.findMany();
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-"></main>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-">
+      <h1>Media Items: {media.length}</h1>
+    </main>
   );
 }
