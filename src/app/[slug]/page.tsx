bc3e480626186ed577ea56efbfdd3ee9b59e3f0c
Kamran Kiani <kiani.km@gmail.com>
2025-10-13 12:57:06 +0330

    working with prisma and supabase

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import Image from "next/image";
+import Link from "next/link";
+
+type Thumbnail = {
+  trending: {
+    small: string;
+    large: string;
+  };
+  regular: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+};
+
+export default async function MediaPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const media = await prisma.media.findUnique({
+    where: {
+      slug: params.slug,
+    },
+  });
+
+  const thumbnail = media?.thumbnail as Thumbnail;
+  return (
+    <main className="flex flex-col gap-[32px]s items-center gap-5 pt-24">
+      <h1 className="text-32 font-medium">{media?.title}</h1>
+
+      <Image
+        src={thumbnail.regular.large?.replace("./assets/", "/assets/")}
+        alt=""
+        width={400}
+        height={400}
+      />
+      <p>{}</p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function Home() {
-  const media = await prisma.media.findMany();
+  const medias = await prisma.media.findMany({
+    take: 10,
+  });
+
+  const mediasCount = await prisma.media.count();
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-">
-      <h1>Media Items: {media.length}</h1>
+    <main className="flex flex-col gap-[32px]s items-center gap-5 pt-24">
+      <h1 className="text-32 font-medium">Media Items: {mediasCount}</h1>
+
+      <ul className="border-t border-b border-black/10 py-5 leading-8">
+        {medias.map((media) => (
+          <li key={media.id} className="flex items-center justify-between px-5">
+            <Link href={`/${media.slug}`}>{media.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
